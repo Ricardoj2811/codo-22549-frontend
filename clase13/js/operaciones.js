fd28f2af203198f03a012616e639f1e958614871
Ricardo Nuñez <ricardoj2811@gmail.com>
2022-09-15 19:57:53 -0300

    Introduccion a JS

---
+++ clase13/js/operaciones.js
@@ -0,0 +1,17 @@
+// Funcion agregada desde hoja externa
+function saludar3(){
+    alert('Hola mundo, de la tercera forma');
+}
+
+// OPERADORES
+// CUENTAS MATEMATICAS
+// +, - , / , *
+
+// Variables
+// Sumar dos valores y mostrar su resultado
+let op1= prompt('Ingrese el valor de op1');
+let op2= prompt('Ingrese el valor de op2');
+let res;
+res = (+op1) + parseInt(op2);
+console.log(res);
+alert('resultado=' + res);
